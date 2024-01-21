2a5963f8850f3fc7a3d675438f5ea9202f0993f4
tangjinlog <lkg70007@gmail.com>
2024-01-22 02:23:30 +0900

    feat: Button, LinkButton atom 컴포넌트 구현 [#1]

---
+++ src/components/atoms/Button/Button.tsx
@@ -0,0 +1,29 @@
+interface ButtonPropTypes
+	extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+	children: React.ReactNode;
+	primary?: boolean;
+	size?: 'big' | 'mid';
+}
+
+const sizes = { big: `w-40 h-20`, mid: `w-24 h-12` };
+
+const buttonStyle = {
+	primary: [`p-1 text-white bg-[#5295FF]`],
+};
+
+function Button({ children, ...props }: ButtonPropTypes) {
+	return (
+		<button
+			{...props}
+			type="button"
+			className={`flex-center border-none rounded-lg cursor-pointer
+        ${props.primary ? buttonStyle.primary : ''}
+        ${props.size ? sizes[props.size] : ''}
+      `}
+		>
+			{children}
+		</button>
+	);
+}
+
+export default Button;
