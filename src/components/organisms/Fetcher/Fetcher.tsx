474c1f18e2a7b95859910114c13e0d3bba164d81
tangjinlog <lkg70007@gmail.com>
2024-01-23 02:20:26 +0900

    feat: Fetcher organism 구현 [#2]

---
+++ src/components/organisms/Fetcher/Fetcher.tsx
@@ -0,0 +1,28 @@
+import { useFetchWikiList } from '@queries/wiki/hooks';
+import { useEffect, useState } from 'react';
+import { wikiListState } from 'context/atom';
+import { useSetRecoilState } from 'recoil';
+
+export function WikiListItemFetcher({
+	children,
+}: {
+	children: React.ReactNode | Function;
+}) {
+	const setList = useSetRecoilState(wikiListState);
+	const [page, setPage] = useState(1);
+	//TODO: ReactQuery 적용
+	const { data, status, isFetching } = useFetchWikiList(page);
+	console.log('ho');
+	useEffect(() => {
+		console.log(`data`, data);
+		setList(data);
+	}, [data]);
+
+	if (status === 'error') {
+		throw new Error('목록을 불러오는 도중 에러가 발생했습니다.');
+	}
+	const toRender =
+		typeof children === 'function' ? children(isFetching, setPage) : children;
+
+	return <>{toRender}</>;
+}
