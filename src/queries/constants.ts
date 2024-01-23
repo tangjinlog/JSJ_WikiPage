41d65f85c4564baec010b7010836963a2bd9283a
tangjinlog <lkg70007@gmail.com>
2024-01-23 23:47:44 +0900

    chore: db 구조 수정 및 쿼리 키 추가 [#2]

--- src/queries/constants.ts
+++ src/queries/constants.ts
@@ -1,4 +1,6 @@
 export const wikiKey = {
 	list: () => ['wikiList'] as const,
 	item: () => ['wikiItem'] as const,
+	post: () => ['wikiItemPost'] as const,
+	patch: () => ['wikiItemPatch'] as const,
 };
