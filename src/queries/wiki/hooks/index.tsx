53a3cfea29650ae60810d535f58c904575a1d3da
tangjinlog <lkg70007@gmail.com>
2024-01-23 23:48:53 +0900

    fix: Query & api 수정 [#2]

--- src/queries/wiki/hooks/index.tsx
+++ src/queries/wiki/hooks/index.tsx
@@ -1,15 +1,56 @@
-import { useQuery, keepPreviousData } from '@tanstack/react-query';
+import { useQuery, keepPreviousData, useMutation } from '@tanstack/react-query';
 import { orderHandlers } from '@queries/wiki';
 import { wikiKey } from '@queries/constants';
-import type { WikiListResponse } from '@queries/wiki';
+import { useQueryClient } from '@tanstack/react-query';
+import type { WikiListResponse, WikiListTypes } from '@queries/wiki';
 
-export const useFetchWikiList = (page: number) => {
-	const { data, status, isFetching } = useQuery<WikiListResponse>({
-		queryKey: [...wikiKey.list(), page],
-		queryFn: () => orderHandlers.getWikiList(page),
-		placeholderData: keepPreviousData,
-		staleTime: 5000,
+export const useFetchWikiList = (page: number = 1) => {
+	const { data, status, isFetching, isPlaceholderData } =
+		useQuery<WikiListResponse>({
+			queryKey: [...wikiKey.list(), page],
+			queryFn: () => orderHandlers.getWikiList(page),
+			placeholderData: keepPreviousData,
+			staleTime: 5000,
+		});
+
+	const pageInfo = {
+		totalPages: data && data?.pages,
+		currentPage: data && (data.next == null ? data.last : data.next - 1),
+		hasMore: data && !!data?.next,
+		lastIdx: data && data.items,
+	};
+
+	console.log(pageInfo);
+	console.log(data);
+	return {
+		data: data?.data.slice(0, 5),
+		status,
+		isFetching,
+		isPlaceholderData,
+		pageInfo,
+	};
+};
+
+export const usePostWikiItem = () => {
+	const queryClient = useQueryClient();
+	const { data, mutateAsync, isPending } = useMutation({
+		mutationKey: [...wikiKey.post()],
+		mutationFn: (wiki: WikiListTypes) => orderHandlers.postWikiItem(wiki),
+		onSuccess: async () => {
+			queryClient.invalidateQueries({ queryKey: [...wikiKey.list()] });
+		},
 	});
+	return { data, mutateAsync, isPending };
+};
 
-	return { data, status, isFetching };
+export const usePatchWikiItem = () => {
+	const queryClient = useQueryClient();
+	const { data, mutateAsync, isPending } = useMutation({
+		mutationKey: [...wikiKey.patch()],
+		mutationFn: (wiki: WikiListTypes) => orderHandlers.patchWikiItem(wiki),
+		onSuccess: async () => {
+			queryClient.invalidateQueries({ queryKey: [...wikiKey.list()] });
+		},
+	});
+	return { data, mutateAsync, isPending };
 };
--- src/queries/wiki/index.ts
+++ src/queries/wiki/index.ts
@@ -1,41 +1,61 @@
 import api from 'queries/config';
 
-type WikiListTypes = {
+interface JsonServerResponse {
+	first: number;
+	items: number;
+	last: number;
+	next: number | null;
+	pages: number;
+	prev: number | null;
+}
+
+export interface WikiListTypes {
 	id: string;
 	author: string;
 	title: string;
 	content: string;
-};
+}
 
-export interface WikiListResponse {
-	id: string;
-	title: string;
-	wiki: WikiListTypes[];
+export interface WikiListResponse extends JsonServerResponse {
+	data: WikiListTypes[];
 }
 
 const getWikiList = async (page: number = 1): Promise<WikiListResponse> => {
-	const currentUrl = window.location.pathname;
 	const defaultLenth = 5;
 	try {
-		const res = await api.get(
-			`${currentUrl}/?tab=wiki&_page=${page}&_per_page=${defaultLenth}`,
-		);
+		const res = await api.get(`/wiki?_page=${page}&_per_page=${defaultLenth}`);
+		console.log(res);
 		return res.data;
 	} catch (error) {
 		throw error;
 	}
 };
 
-type GetWikiItemTypes = { id: string; title: string };
-
-const getWikiItem = async (
-	wiki: GetWikiItemTypes,
-): Promise<WikiListResponse['wiki']> => {
-	const currentUrl = window.location.pathname;
+const postWikiItem = async (wiki: WikiListTypes): Promise<WikiListResponse> => {
 	try {
-		const res = await api.get(
-			`${currentUrl}/?tab=wiki&id=${wiki.id}&title=${wiki.title}`,
-		);
+		const res = await api.post(`/wiki`, {
+			id: wiki.id,
+			author: '정상진',
+			title: wiki.title,
+			content: wiki.content,
+		});
+		console.log('wiki post', res.data);
+		return res.data;
+	} catch (error) {
+		throw error;
+	}
+};
+
+const patchWikiItem = async (
+	wiki: WikiListTypes,
+): Promise<WikiListResponse> => {
+	try {
+		const res = await api.patch(`/wiki/${wiki.id}`, {
+			id: wiki.id,
+			author: '정상진',
+			title: wiki.title,
+			content: wiki.content,
+		});
 		return res.data;
 	} catch (error) {
 		throw error;
@@ -44,82 +64,6 @@ const getWikiItem = async (
 
 export const orderHandlers = {
 	getWikiList,
-	getWikiItem,
+	postWikiItem,
+	patchWikiItem,
 };
-
-////
-
-// async function fetchProjects(page = 0) {
-//   const { data } = await axios.get('/api/projects?page=' + page)
-//   return data
-// }
-
-// function Example() {
-//   const queryClient = useQueryClient()
-//   const [page, setPage] = React.useState(0)
-
-//   const { status, data, error, isFetching, isPreviousData } = useQuery({
-//     queryKey: ['projects', page],
-//     queryFn: () => fetchProjects(page),
-//     keepPreviousData: true,
-//     staleTime: 5000,
-//   })
-
-//   // Prefetch the next page!
-//   React.useEffect(() => {
-//     if (!isPreviousData && data?.hasMore) {
-//       queryClient.prefetchQuery({
-//         queryKey: ['projects', page + 1],
-//         queryFn: () => fetchProjects(page + 1),
-//       })
-//     }
-//   }, [data, isPreviousData, page, queryClient])
-
-//   return (
-//     <div>
-//       <p>
-//         In this example, each page of data remains visible as the next page is
-//         fetched. The buttons and capability to proceed to the next page are also
-//         supressed until the next page cursor is known. Each page is cached as a
-//         normal query too, so when going to previous pages, you'll see them
-//         instantaneously while they are also refetched invisibly in the
-//         background.
-//       </p>
-//       {status === 'loading' ? (
-//         <div>Loading...</div>
-//       ) : status === 'error' ? (
-//         <div>Error: {error.message}</div>
-//       ) : (
-//         // `data` will either resolve to the latest page's data
-//         // or if fetching a new page, the last successful page's data
-//         <div>
-//           {data.projects.map((project) => (
-//             <p key={project.id}>{project.name}</p>
-//           ))}
-//         </div>
-//       )}
-//       <div>Current Page: {page + 1}</div>
-//       <button
-//         onClick={() => setPage((old) => Math.max(old - 1, 0))}
-//         disabled={page === 0}
-//       >
-//         Previous Page
-//       </button>{' '}
-//       <button
-//         onClick={() => {
-//           setPage((old) => (data?.hasMore ? old + 1 : old))
-//         }}
-//         disabled={isPreviousData || !data?.hasMore}
-//       >
-//         Next Page
-//       </button>
-//       {
-//         // Since the last page's data potentially sticks around between page requests,
-//         // we can use `isFetching` to show a background loading
-//         // indicator since our `status === 'loading'` state won't be triggered
-//         isFetching ? <span> Loading...</span> : null
-//       }{' '}
-//       <ReactQueryDevtools initialIsOpen />
-//     </div>
-//   )
-// }
