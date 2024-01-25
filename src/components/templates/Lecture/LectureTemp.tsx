29c2df51042fc617ac352fc376f7b249ba0af052
tangjinlog <lkg70007@gmail.com>
2024-01-26 02:01:12 +0900

    feat: Home 버튼 추가 [#2]

--- src/components/templates/Lecture/LectureTemp.tsx
+++ src/components/templates/Lecture/LectureTemp.tsx
@@ -4,7 +4,13 @@ function LectureTemp() {
 	return (
 		<section className="full-screen flex">
 			<div className="flex-center flex-1 p-6 bg-gray-50">
-				<div className="flex-center w-full h-full sm:h-[calc(100vh-104px)] sm:mb-[56px] rounded-xl bg-gray-200">
+				<div className="relative flex-center w-full h-full sm:h-[calc(100vh-104px)] sm:mb-[56px] rounded-xl bg-gray-200">
+					<a
+						href="/"
+						className="absolute top-0 left-0 px-2 rounded-md bg-black/[0.2] text-white"
+					>
+						home button
+					</a>
 					Loading...
 				</div>
 			</div>
