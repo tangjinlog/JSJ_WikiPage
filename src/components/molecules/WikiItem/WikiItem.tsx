615a3f54b72fb69479e2c318c477a536dc846998
tangjinlog <lkg70007@gmail.com>
2024-01-29 02:04:57 +0900

    chore: dompurify 패키지 설치 [#2]

--- src/components/molecules/WikiItem/WikiItem.tsx
+++ src/components/molecules/WikiItem/WikiItem.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import DOMPurify from 'dompurify';
 
 const Count = ({ children }: { children: React.ReactNode }) => {
 	return <div className="text-sm text-gray-500">{children}</div>;
@@ -8,8 +8,15 @@ const Title = ({ children }: { children: React.ReactNode }) => {
 	return <h3 className="text-lg">{children}</h3>;
 };
 
-const Content = ({ children }: { children: React.ReactNode }) => {
-	return <div className="text-m whitespace-pre-wrap py-4">{children}</div>;
+const Content = ({ content }: { content: string }) => {
+	return (
+		<div
+			className="text-m whitespace-pre-wrap py-4"
+			dangerouslySetInnerHTML={{
+				__html: DOMPurify.sanitize(content),
+			}}
+		/>
+	);
 };
 
 const Author = ({ children }: { children: React.ReactNode }) => {
